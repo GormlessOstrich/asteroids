b1c9bf16b1bb51d266d8cce617f832a10edc0732
GormlessOstrich <gormlessostrich@gmail.com>
2025-07-15 15:28:28 +0100

    C: Added player movement.

--- main.py
+++ main.py
@@ -9,6 +9,12 @@ def main():
 
     pygame.init()
 
+    # Groups
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    # Adding both groups to a container
+    Player.containers = (updatable, drawable)
+
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -18,9 +24,15 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        pygame.Surface.fill(screen, "black")
-        player.draw(screen)
+
+        updatable.update(dt)
+        
+        # Replaces 'pygame.Surface.fill(screen, "black")'
+        screen.fill("black")
+        for thing in drawable:
+            thing.draw(screen)
         pygame.display.flip()
+        # Limits the framerate to 60 FPS.
         dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
